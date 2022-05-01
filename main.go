cb7a10390b82144f92a88a65baf6f0b383341f39
DaisukeMatsuura <mail@brightful.jp>
2022-05-01 09:12:16 +0900

    add: PUT & DELETE method

--- main.go
+++ main.go
@@ -35,12 +35,12 @@ func main() {
 	})
 	e.GET("/product/:id", func(c echo.Context) error {
 		var product map[int]string
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
 		for _, p := range products {
 			for k := range p {
-				pID, err := strconv.Atoi(c.Param("id"))
-				if err != nil {
-					return err
-				}
 				if pID == k {
 					product = p
 				}
@@ -69,6 +69,66 @@ func main() {
 			len(products) + 1: reqBody.Name,
 		}
 		products = append(products, product)
+		return c.JSON(http.StatusOK, product)
+	})
+	e.PUT("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
+		for _, p := range products {
+			for k := range p {
+				if pID == k {
+					product = p
+				}
+			}
+		}
+
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "product not found.")
+		}
+
+		type body struct {
+			Name string `json:"product_name" validate:"required,min=4"`
+		}
+		var reqBody body
+		e.Validator = &ProductValidator{validator: v}
+		if err := c.Bind(&reqBody); err != nil {
+			return err
+		}
+		if err := c.Validate(reqBody); err != nil {
+			return err
+		}
+
+		product[pID] = reqBody.Name
+		return c.JSON(http.StatusOK, product)
+	})
+
+	e.DELETE("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		var index int
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return nil
+		}
+		for i, p := range products {
+			for k := range p {
+				if pID == k {
+					product = p
+					index = i
+				}
+			}
+		}
+
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "product not found.")
+		}
+		splice := func (s []map[int]string, index int) []map[int]string {
+			return append(s[:index], s[index+1:]...)
+		}
+		products = splice(products, index)
+
 		return c.JSON(http.StatusOK, product)
 	})
 
